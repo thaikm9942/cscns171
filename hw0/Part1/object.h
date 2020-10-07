7b5e48635f3fe7f243257efc959c09b5b78e77ef
tkhong <tkhong@caltech.edu>
2020-10-06 21:55:01 -0700

    changed back .hpp to .h extension for part1, updated header files to define new common classes and functions, and fixed parse.cpp to use the new classes

---
+++ hw0/Part1/object.h
@@ -0,0 +1,60 @@
+#ifndef __OBJECT_H__
+#define __OBJECT_H__
+
+#include <vector>
+#include "../Eigen/Dense"
+
+using namespace Eigen;
+using namespace std;
+
+// This class represents a vertex with x, y, and z float coordinates 
+class Vertex {
+    public:
+        float x_, y_, z_;
+        // Default constructor
+        Vertex() : x_(0.0), y_(0.0), z_(0.0) {}
+
+        // Constructor that takes in a given (x, y, z) coordinates
+        // to form a vertex
+        Vertex(float x, float y, float z) : x_(x), y_(y), z_(z) {}
+};
+
+/* This class represents a face containing 3 integers representing 
+ * the 3 1-indexed vertices forming that face.
+ */ 
+class Face {
+    public:
+        int i1_, i2_, i3_;
+        
+        // Constructor that takes in 3 integers corresponding to
+        // the 3 indices of vertices that form the face
+        Face(int i1, int i2, int i3) : i1_(i1), i2_(i2), i3_(i3) {}
+};
+
+/* This class represents a graphics object which contains the vertices,
+ * the faces of an object, and the transformations to be applied to each
+ * vertex 
+ */
+class Object {
+    public:
+        vector<Vertex> vertices;
+        vector<Face> faces;
+        vector<Matrix4d> transforms;
+
+        // Default constructor that initializes empty lists of vertices,
+        // faces and transforms for the object
+        Object() : vertices(), faces(), transforms() {}
+
+        // Constructor that takes in only a list of vertices and faces that
+        // forms the object. The list of transformation matrices are initialized,
+        // but left empty.
+        Object(vector<Vertex> vtx, vector<Face> fs) : vertices(vtx), faces(fs), transforms() {}
+
+        // Functions to add a vertex, a face or a transformation matrix to the corresponding
+        // list of the object
+        void add_vertex(Vertex v) { vertices.push_back(v); }
+        void add_face(Face f) { faces.push_back(f); }
+        void add_transform(Matrix4d m) {transforms.push_back(m); }
+};
+
+#endif // #ifndef __OBJECT_H__
--- hw0/Part1/parse.cpp
+++ hw0/Part1/parse.cpp
@@ -1,36 +1,25 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <math.h>
 #include "object.h"
-
-using namespace std;
-
-/* This struct represents a graphics object, which contains a vector of
- * all vertices in the object and a vector of faces forming the object
- */
-struct Object {
-    vector<Vertex> vertices;
-    vector<Face> faces;
-};
 
 /* Default NULL_VERTEX to mark the start of a vector of Vertex objects
  * since vertices are 1-indexed
  */
-const Vertex NULL_VERTEX = {INFINITY, INFINITY};
+const Vertex NULL_VERTEX = Vertex(INFINITY, INFINITY, INFINITY);
 
 // Prints in the object to the output in a formatted paragraph
 void print_object(Object obj) {
     // Enumerates all the vertices in the object
     for (int i = 1; i < obj.vertices.size(); i++) {
         Vertex v = obj.vertices[i];
-        printf("v %f %f %f\n", v.x, v.y, v.z);
+        printf("v %f %f %f\n", v.x_, v.y_, v.z_);
     }
     // Enumerates all the faces in the object
     for (int i = 0; i < obj.faces.size(); i++) {
         Face f = obj.faces[i];
-        printf("f %d %d %d\n", f.i1, f.i2, f.i3);
+        printf("f %d %d %d\n", f.i1_, f.i2_, f.i3_);
     }
 }
 
@@ -39,10 +28,8 @@ void print_object(Object obj) {
 Object create_object(ifstream &ifs) {
     // Initializes the vectors containing the corresponding vertices and faces
     // of an object
-    vector<Vertex> obj_vertices;
-    vector<Face> obj_faces;
-
-    obj_vertices.push_back(NULL_VERTEX);
+    Object obj = Object();
+    obj.add_vertex(NULL_VERTEX);
 
     // Variables to store the tokens from the file
     // The "type" of data, either a face or a vertex
@@ -57,21 +44,20 @@ Object create_object(ifstream &ifs) {
         // If the first character of the line is 'v', then process the
         // strings as vertex float coordinates 
         if (type == 'v') {
-            Vertex v = {strtof(n1.c_str(), NULL), strtof(n2.c_str(), NULL), strtof(n3.c_str(), NULL)};
-            obj_vertices.push_back(v);
+            Vertex v = Vertex(strtof(n1.c_str(), NULL), strtof(n2.c_str(), NULL), strtof(n3.c_str(), NULL));
+            obj.add_vertex(v);
         }
         // If the first character is 'f' instead, then process the strings
         // as the vertex integer indices
         else if (type == 'f') {
-            Face f = {atoi(n1.c_str()), atoi(n2.c_str()), atoi(n3.c_str())};
-            obj_faces.push_back(f);
+            Face f = Face(atoi(n1.c_str()), atoi(n2.c_str()), atoi(n3.c_str()));
+            obj.add_face(f);
         }
         else {
             cout << "Data is neither a vertex or a file\n";
             break;
         }
     }
-    Object obj = {obj_vertices, obj_faces};
     return obj;
 }
 
